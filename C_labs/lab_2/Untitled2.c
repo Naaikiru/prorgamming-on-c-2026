fc7adb4b041e74684b11d52c98febaa6a21eb7ec
Naaikiru <kiki010907@mail.ru>
2026-03-20 00:09:21 +1000

    ghgh

--- C_labs/lab_2/Untitled2.c
+++ C_labs/lab_2/Untitled2.c
@@ -16,20 +16,3 @@ int main(void)
 	return 0;
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-int main(void) 
-{
-	
-	double m[3][3];
-	
-	scanf("%lf \n", &m[1][0]);
-	scanf("%lf", &m[2][0]);
-
-	printf("%lf \n", m[1][0]);
-	printf("%lf \n", m[2][0]);
-	
-	return 0;
-}
