5413604092257b28ade5ab243b9543ea29e35652
Naaikiru <kiki010907@mail.ru>
2026-03-19 21:31:52 +1000

    meow

---
+++ C_labs/lab_2/Untitled2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void) 
+{
+	
+	double m[3][3];
+	
+	scanf("%lf \n", &m[1][0]);
+	scanf("%lf", &m[2][0]);
+
+	printf("%lf \n", m[1][0]);
+	printf("%lf \n", m[2][0]);
+	
+	return 0;
+}
