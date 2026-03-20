81d3cd877a2736bdae895e55cd6d30244958c444
Naaikiru <kiki010907@mail.ru>
2026-03-21 01:59:21 +1000

    adsasad

--- C_labs/lab_2/Untitled2.c
+++ C_labs/lab_2/Untitled2.c
@@ -4,20 +4,42 @@
 
 int main(void) 
 {
-	
+	printf("matrices with the 'double' type(3x3): \n");
+
 	double m[3][3];
 	int x;
 	int y;
     for(x =0; x < 3; x++)
     {
-        for(y =0; y<3; y++)
+        for(y =0; y < 3; y++)
         {
             scanf("%lf \n", &m[x][y]);
         }
     }
 
-    printf("%lf \n", m[0][0]);
+    printf("the sum of the values of the main diagonal = %lf \n", m[0][0]+m[1][1]+m[2][2]);
+    printf("the sum of the values of the side diagonal = %lf \n", m[0][2]+m[1][1]+m[2][0]);
 
+	printf("matrices with the 'int' type(2x2): \n");
+
+    int mm[2][2];
+	int xx;
+	int yy;
+    for(xx =0; xx < 2; xx++)
+    {
+        for(yy =0; yy < 2; yy++)
+        {
+            scanf("%d \n", &mm[xx][yy]);
+        }
+    }
+
+	printf("the square of the second matrix: \n");
+    printf("%d", (mm[0][0]*mm[0][0])+(mm[0][1]*mm[1][0]));
+	printf(" %d \n", (mm[0][0]*mm[0][1])+(mm[0][1]*mm[1][1]));
+    printf("%d", (mm[0][0]*mm[1][0])+(mm[1][1]*mm[1][0]));
+	printf(" %d \n", (mm[1][0]*mm[0][1])+(mm[1][1]*mm[1][1]));
+
+   
 	return 0;
 }
 
--- C_labs/lab_3/lab3.c
+++ C_labs/lab_3/lab3.c
@@ -4,14 +4,23 @@
 
 int main(void) 
 {
-	
-	double m[3][3];
-	
-	scanf("%lf \n", &m[1][0]);
-	scanf("%lf", &m[2][0]);
+    int mm[2][2];
+	int xx;
+	int yy;
+    for(xx =0; xx < 2; xx++)
+    {
+        for(yy =0; yy < 2; yy++)
+        {
+            scanf("%d \n", &mm[xx][yy]);
+        }
+    }
 
-	printf("%lf \n", m[1][0]);
-	printf("%lf \n", m[2][0]);
-	
+	printf("the square of the second matrix: \n");
+    printf("%d", (mm[0][0]*mm[0][0])+(mm[0][1]*mm[1][0]));
+	printf(" %d \n", (mm[0][0]*mm[0][1])+(mm[0][1]*mm[1][1]));
+    printf("%d", (mm[0][0]*mm[1][0])+(mm[1][1]*mm[1][0]));
+	printf(" %d \n", (mm[1][0]*mm[0][1])+(mm[1][1]*mm[1][1]));
+
+   
 	return 0;
 }
