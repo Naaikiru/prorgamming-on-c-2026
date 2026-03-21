2d73523695bbe1fabb4f59010b295c208a49038d
Naaikiru <kiki010907@mail.ru>
2026-03-21 13:19:26 +1000

    выфвв

--- C_labs/lab_2/lab2.c
+++ C_labs/lab_2/lab2.c
@@ -11,57 +11,64 @@ int main(void)
         printf("enter the size of the matrix: \n");
         scanf("%d", &size);
 
-        if (size == 3)
-        {
-            printf("matrices with the 'double' type(3x3): \n");
 
-            double m[size][size];
-            int x;
-            int y;
-            double sum1, sum2;
-            for(x =0; x < size; x++)
+        printf("matrices with the 'double' type(3x3): \n");
+
+        double m[size][size];
+        int x;
+        int y;
+        double sum1= 0, sum2= 0;
+        for(x =0; x < size; x++)
+        {
+            for(y =0; y < size; y++)
             {
-                for(y =0; y < size; y++)
-                {
-                    scanf("%lf", &m[x][y]);
-                }
+                scanf("%lf", &m[x][y]);
             }
-            for (int s = 0; s < size; s++)
-            {
-                sum1 += m[s][s];
-                sum2 += m[s][2-s];
-            }
-                           
-            printf("the sum of the values of the main diagonal = %lf \n", sum1);
-            printf("the sum of the values of the side diagonal = %lf \n", sum2);
         }
-        
-        /*else if (size == 2)
+
+        for (int s = 0; s < size; s++)
         {
-            printf("matrices with the 'int' type(2x2): \n");
+            sum1 += m[s][s];
+            sum2 += m[s][size-1-s];
+      
+        }
+                           
+        printf("the sum of the values of the main diagonal = %lf \n", sum1);
+        printf("the sum of the values of the side diagonal = %lf \n", sum2);
+        
+        
 
-            int mm[size][size];
-            int xx;
-            int yy;
-            for(xx =0; xx < size; xx++)
+        printf("matrices with the 'int' type(2x2): \n");
+        double array [2][2];
+        double sqr [2][2];
+        for (int i = 0; i < 2; i++) 
+        {
+            for (int j = 0; j < 2; j++) 
             {
-                for(yy =0; yy < size; yy++)
-                {
-                    scanf("%d", &mm[xx][yy]);
-                }
+                scanf("%lf", &array [i][j]);
             }
+        }
 
-            printf("the square of the second matrix: \n");
+        sqr [0][0] = array [0][0] * array [0][0] + array [0][1] * array [1][0];
+        sqr [0][1] = array [0][0] * array [0][1] + array [0][1] * array [1][1];
+        sqr [1][0] = array [1][0] * array [0][0] + array [1][1] * array [1][0];
+        sqr [1][1] = array [1][0] * array [0][1] + array [1][1] * array [1][1];
+        
+        for(x =0; x < size; x++)
+        {
+            for(y =0; y < size; y++)
+            {
+                printf ("%lf", sqr([x][y]);
+                printf(" ");
+            }
+        }
+
+            /*printf("the square of the second matrix: \n");
             printf("%d", (mm[0][0]*mm[0][0])+(mm[0][1]*mm[1][0]));
             printf(" %d \n", (mm[0][0]*mm[0][1])+(mm[0][1]*mm[1][1]));
             printf("%d", (mm[0][0]*mm[1][0])+(mm[1][1]*mm[1][0]));
-            printf(" %d \n", (mm[1][0]*mm[0][1])+(mm[1][1]*mm[1][1]));
-        }
+            printf(" %d \n", (mm[1][0]*mm[0][1])+(mm[1][1]*mm[1][1]));*/
 
-        else
-        {
-            printf("You have entered the wrong value, please try again! \n");
-        }*/
 
         int z;
         printf(" \n");
